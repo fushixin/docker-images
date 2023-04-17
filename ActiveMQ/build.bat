279ead167f9813231ec81b17042fec59d51d60d5
fushixin <fushixin.vip@gmail.com>
2023-04-17 17:11:41 +0800

    update

--- ActiveMQ/build.bat
+++ ActiveMQ/build.bat
@@ -1,7 +1,11 @@
 @echo off
 
-call docker build -f Dockerfile -t fushixin/activemq:5.16.6 .
+call docker build -f Dockerfile -t fushixin/activemq:latest .
+
+call docker tag fushixin/activemq:latest fushixin/activemq:5.16.6
 
 call docker push fushixin/activemq:5.16.6
+
+call docker push fushixin/activemq:latest
 
 exit /b 0
